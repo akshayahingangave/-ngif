0483911def992ae912ee71556c6d7304653bdac6
Akshaya <akshayarh@gmail.com>
2021-04-04 17:09:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export class User {
+    public name: any;
+    public email: any;
+    public password: any;
+    public hobbies: any;
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ngif';
+    model = new User();
+
+    Hobbies: string[] = [
+        'Acrobatics',
+        'Acting',
+        'Animation',
+        'Astronomy',
+        'Baking'
+    ];
+
+    constructor() { }
+
+    onSubmit(form: { value: any; }) {
+        console.log(form.value)
+    }
 }
